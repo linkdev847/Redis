3109cafa46f8da0a538a3521ff9b59953cdb2d22
Ashraf Shaker <Ashraf.Shaker@linkdev.com>
2025-01-09 11:29:12 +0200

    add files

--- Redis/Program.cs
+++ Redis/Program.cs
@@ -1,2 +1,87 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+using RedisCashing.Services;
+using System;
+using System.Diagnostics;
+
+namespace Redis.ConsoleApp
+{
+    class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            //TestRedis();
+
+            double latitude = 39.7456;
+            double longitude = -97.0891;
+
+            string json;
+            var watch = Stopwatch.StartNew();
+            var keyName = $"forecast:{latitude},{longitude}";
+
+
+            var _redis = new Rediservice().redis;
+
+            json = await _redis.StringGetAsync(keyName);
+
+
+            if (string.IsNullOrEmpty(json))
+            {
+                var forecastService = new ForecastService();
+                json = await forecastService.GetForecast(latitude, longitude);
+                var setTask = _redis.StringSetAsync(keyName, json);
+                var expireTask = _redis.KeyExpireAsync(keyName, TimeSpan.FromSeconds(3600));
+                await Task.WhenAll(setTask, expireTask);
+            }
+            else
+            {
+                Console.WriteLine("get from Redis Cash");
+
+            }
+            var forecast = JsonConvert.DeserializeObject<IEnumerable<WeatherForecast>>(json);
+
+            watch.Stop();
+
+            Console.WriteLine(json);
+            Console.WriteLine(watch.ElapsedMilliseconds);
+
+        }
+
+        private static void TestRedis()
+        {
+            var _redis = new Rediservice().redis;
+
+            //Simple Ping command to check if Database is Up, if database is up response will be pong
+            var commandresponse = _redis.Execute("PING");
+            Console.WriteLine(commandresponse.ToString());
+
+            //Set a Value in Cache
+            _redis.StringSet("TestConsole", "Hello from Console App, how are you doing");
+
+            //Read recently setted Value From Cache
+            var cachedresponse = _redis.StringGet("TestConsole");
+            Console.WriteLine(cachedresponse.ToString());
+
+            //Add an Object to Redis
+            Employee e1 = new Employee() { Id = 1, Name = "Console Employee" };
+            var jsonString = JsonConvert.SerializeObject(e1);
+            _redis.StringSet("e1", jsonString);
+
+            //Read An Object From redis
+            cachedresponse = _redis.StringGet("e1");
+            var employee = JsonConvert.DeserializeObject<Employee>(cachedresponse.ToString());
+            Console.WriteLine($"Employee Id from Cache: {employee.Id}");
+            Console.WriteLine($"Employee Name from Cache: {employee.Name}");
+
+            Console.WriteLine("Press Any Key to exit....");
+            Console.ReadLine();
+        }
+
+        public class Employee
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+    }
+}
---
+++ Redis/Services/ForecastService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace RedisCashing.Services
+{
+    public class ForecastService
+    {
+        public HttpClient _client { get; set; } = new HttpClient();
+
+
+        public async Task<string> GetForecast(double latitude, double longitude)
+        {
+            var pointsRequestQuery = $"https://api.weather.gov/points/{latitude},{longitude}"; //get the URI
+            _client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("weatherCachingApp", "1.0"));
+            var result = await _client.GetFromJsonAsync<JsonObject>(pointsRequestQuery);
+            var gridX = result["properties"]["gridX"].ToString();
+            var gridY = result["properties"]["gridY"].ToString();
+            var gridId = result["Properties"]["gridId"].ToString();
+            var forecastRequestQuery = $"https://api.weather.gov/gridpoints/{gridId}/{gridX},{gridY}/forecast";
+            var forecastResult = await _client.GetFromJsonAsync<JsonObject>(forecastRequestQuery);
+            var periodsJson = forecastResult["properties"]["periods"].ToJsonString();
+            return periodsJson;
+        }
+    }
+}
